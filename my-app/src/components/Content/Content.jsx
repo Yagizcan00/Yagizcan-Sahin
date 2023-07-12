ee35075938c9997575ce0c7dda80f190b606d845
Yağızcan Şahin <85365132+Yagizcan00@users.noreply.github.com>
2023-07-12 11:47:21 +0300

    Update

--- my-app/src/components/Content/Content.jsx
+++ my-app/src/components/Content/Content.jsx
@@ -7,10 +7,12 @@ function Content() {
         <div className='content'>
 
             <section className='about_myself'>
+
                 <section className='heading_about_myself'>
                     <section className='dot' />
                     <h2>About Myself</h2>
                 </section>
+
                 <section className='content_about_myself'>
                     <p>I will briefly tell you about myself.
                         I love coding, creativity, analysis and football;
@@ -22,6 +24,177 @@ function Content() {
                         day.
                     </p>
                 </section>
+
+            </section>
+
+            <hr style={{ width: "80vw" }} />
+
+            <section className='education'>
+
+                <section className='heading_education'>
+                    <section className='dot' />
+                    <h2>Education</h2>
+                </section>
+
+                <section className='content_education'>
+                    <h3>Kağıthane Anatolian High School</h3>
+                    <p>2014 - 2018</p>
+                    <h3>Doğuş University</h3>
+                    <p>Computer Engineering (English)</p>
+                    <p>2019 - Continues</p>
+                </section>
+
+            </section>
+
+            <hr style={{ width: "80vw" }} />
+
+            <section className='technical_skills'>
+
+                <section className='heading_technical_skills'>
+                    <section className='dot' />
+                    <h2>Technical Skills</h2>
+                </section>
+
+                <section className='content_technical_skills'>
+                    <ul>
+                        <li>React.js</li>
+                        <li>JavaScript</li>
+                        <li>CSS</li>
+                        <li>HTML</li>
+                        <li>State Management (Context)</li>
+                        <li>Project Architecture</li>
+                        <li>Responsive Apps</li>
+                        <li>Firebase</li>
+                        <li>Clean Coding</li>
+                    </ul>
+                </section>
+
+            </section>
+
+            <hr style={{ width: "80vw" }} />
+
+            <section className='my_experience'>
+
+                <section className='heading_my_experience'>
+                    <section className='dot' />
+                    <h2>My Experience</h2>
+                </section>
+
+                <section className='content_my_experience'>
+                    <h3>Volunteer software support worker</h3>
+                    <h4>Inooster I.T. - İstanbul - 2022/2023</h4>
+                    <p>
+                        I worked as a volunteer for more than 9 months
+                        in a software company that produces gamification
+                        software to learn my profession better and improve
+                        myself. Here, I coded the Entry-Exit application in
+                        my projects section by improving myself with the
+                        guidance of experienced software developers, by making
+                        various practices and thanks to my research & learning
+                        skills. In the following process, I continued to work
+                        on front-end.
+                    </p>
+                </section>
+
+            </section>
+
+            <hr style={{ width: "80vw" }} />
+
+            <section className='languages'>
+
+                <section className='heading_languages'>
+                    <section className='dot' />
+                    <h2>Languages</h2>
+                </section>
+
+                <section className='content_languages'>
+                    <ul>
+                        <li>English  (B2 level)</li>
+                        <li>German  (A1 level)</li>
+                        <li>Turkish  (Natural)</li>
+                    </ul>
+                </section>
+
+            </section>
+
+            <hr style={{ width: "80vw" }} />
+
+            <section className='my_projects'>
+
+                <section className='heading_my_projects'>
+                    <section className='dot' />
+                    <h2>My Projects</h2>
+                </section>
+
+                <section className='content_my_projects'>
+                    <>
+                        <h3>Entry-Exit</h3>
+                        <p>
+                            This project was coded to read the entry and
+                            exit times of the employees. While carrying
+                            out this project, I learned how to connect
+                            with the backend using the API, how to use
+                            the react-data-table-component library, etc.
+                            technical features. I developed this project
+                            for my workplace. That's why I can't share it
+                            anywhere.
+                        </p>
+                    </>
+                    <>
+                        <h3>Seven to Seven</h3>
+                        <p>
+                            I coded this project to have more fun while
+                            playing football with my friends. I used the
+                            react-draggable library, the react-router-dom
+                            library, the react-icons library, the react-color
+                            library, the use-react-screenshot library, etc.
+                            The link of this project is in my GitHub account
+                        </p>
+                    </>
+                    <>
+                        <h3>Weather App</h3>
+                        <p>
+                            I coded this project to have more fun while playing
+                            football with my friends. I used the react-draggable
+                            library, the react-router-dom library, the react-icons
+                            library, the react-color library, the use-react-screenshot
+                            library, etc. This project has two different versions.
+                            The first version is available on my GitHub account,
+                            and the second version is a version that I customized
+                            and edited for my girlfriend, if you want the link of
+                            this version, I can share it with you.
+                        </p>
+                    </>
+                    <>
+                        <h3>QR Menu & QR Code Creator</h3>
+                        <p>
+                            I coded this project to complete a freelance job. My
+                            customer asked me for a QR code-enabled menu for
+                            the cafe he owned, and I coded a QR code for his cafe
+                            and the necessary actions to direct the devices that
+                            read this QR code to the Menu site. Then I developed
+                            this Menu application for mobile devices. While
+                            developing these applications, I used the qrcode.react
+                            library and various react methods. The link of my QR
+                            code generator application is in my Github Account,
+                            but since my Menu application is private to my client,
+                            it is privately in my GitHub account but if you want
+                            I can show you an example.
+                        </p>
+                    </>
+                    <>
+                        <h3>Quiz App</h3>
+                        <p>
+                            I developed this project for practice. I'm sure you know
+                            that there is a contest called "Who wants to be a millionaire"
+                            and I developed this web application inspired by this contest.
+                            While developing this application, I used many methods such
+                            as useReducer, useRef, useEffect, etc. The link of the application
+                            is in my GitHub account.
+                        </p>
+                    </>
+                </section>
+
             </section>
 
         </div>
--- my-app/src/components/Heading/Heading.jsx
+++ my-app/src/components/Heading/Heading.jsx
@@ -3,9 +3,28 @@ import "./Heading.scss"
 import { MdEmail } from 'react-icons/md';
 import { AiFillGithub } from 'react-icons/ai';
 import { AiFillLinkedin } from 'react-icons/ai';
+import { BsFillTelephoneFill } from 'react-icons/bs';
 
 
 function Heading() {
+
+
+    const handleClickMail = () => {
+        window.open('https://mail.google.com/mail/u/0/#inbox?compose=new', '_blank');
+    }
+
+    
+    const handleClickGitHub = () => {
+        // window.location.href = "https://github.com/Yagizcan00"
+        window.open('https://github.com/Yagizcan00', '_blank');
+    }
+
+
+    const handleClickLinkedin = () => {
+        // window.location.href = "https://linkedin.com/in/yağızcan-şahin-a00a13200/"
+        window.open('https://linkedin.com/in/yağızcan-şahin-a00a13200/', '_blank');
+    }
+
 
     return (
         <div className='heading'>
@@ -22,20 +41,20 @@ function Heading() {
             <section className='contact'>
 
                 <section className='content_item'>
-                    <section className='icon_background'><MdEmail className='icon' /></section>
+                    <section className='icon_background'><MdEmail className='icon' onClick={handleClickMail} /></section>
                     <p>yagizcansahin68@gmail.com</p>
                 </section>
                 <section className='content_item'>
-                    <section className='icon_background'><AiFillGithub className='icon' /></section>
+                    <section className='icon_background'><AiFillGithub className='icon' onClick={handleClickGitHub} /></section>
                     <p>github.com/Yagizcan00</p>
                 </section>
                 <section className='linkedin'>
-                    <section className='linkedin_icon_background'><AiFillLinkedin className='linkedin_icon' /></section>
+                    <section className='linkedin_icon_background'><AiFillLinkedin className='linkedin_icon' onClick={handleClickLinkedin} /></section>
                     <p>linkedin.com/in/yağızcan-şahin-a00a13200/</p>
                 </section>
                 <section className='content_item'>
-                    <section className='icon_background'><MdEmail className='icon' /></section>
-                    <p>yagizcansahin68@gmail.com</p>
+                    <section className='icon_background'><BsFillTelephoneFill className='icon' /></section>
+                    <p>+90 551 429 88 82</p>
                 </section>
 
             </section>
